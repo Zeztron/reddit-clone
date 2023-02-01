4cdbb57cd93883a5f8e8b119523e23ff3c139930
Zeztron <pateltharsh@gmail.com>
2023-01-31 19:30:29 -0600

    created Header component

---
+++ components/Header.tsx
@@ -0,0 +1,75 @@
+import Image from 'next/image';
+import React from 'react';
+import {
+  BeakerIcon,
+  ChevronDownIcon,
+  HomeIcon,
+  MagnifyingGlassIcon,
+  Bars3Icon,
+} from '@heroicons/react/24/solid';
+import {
+  BellIcon,
+  ChatBubbleOvalLeftEllipsisIcon,
+  PlusIcon,
+  SparklesIcon,
+  VideoCameraIcon,
+  MegaphoneIcon,
+  GlobeAmericasIcon,
+} from '@heroicons/react/24/outline';
+
+const Header = () => {
+  return (
+    <div className="sticky top-0 z-50 flex bg-white p-4 shadow-sm">
+      <div className="relative h-10 w-20 flex-shrink-0 cursor-pointer">
+        <Image
+          style={{ objectFit: 'contain' }}
+          src="https://links.papareact.com/fqy"
+          fill
+          alt="Reddit 2.0"
+        />
+      </div>
+      <div className="flex items-center mx-7 xl:min-w-[300px]">
+        <HomeIcon className="h-5 w-5" />
+        <p className="flex-1 ml-2 hidden lg:inline">Home</p>
+        <ChevronDownIcon className="h-5 w-5" />
+      </div>
+      {/* Search */}
+      <form className="flex flex-1 items-center space-x-2 border border-gray-200 rounded-sm bg-gray-100 px-3 py-1">
+        <MagnifyingGlassIcon className="h-6 w-6 text-gray-400" />
+        <input
+          className="flex-1 bg-transparent outline-none"
+          type="text"
+          placeholder="Search Reddit"
+        />
+        <button type="submit" hidden></button>
+      </form>
+      <div className="text-gray-500x space-x-2 items-center mx-5 hidden lg:inline-flex">
+        <SparklesIcon className="icon" />
+        <GlobeAmericasIcon className="icon" />
+        <VideoCameraIcon className="icon" />
+        <hr className="h-10 border border-gray-100" />
+        <ChatBubbleOvalLeftEllipsisIcon className="icon" />
+        <BellIcon className="icon" />
+        <PlusIcon className="icon" />
+        <MegaphoneIcon className="icon" />
+      </div>
+      <div className="ml-5 flex items-center lg:hidden">
+        <Bars3Icon className="icon" />
+      </div>
+      {/* Sign In/ Sign out button */}
+      <div className="hidden lg:flex items-center space-x-2 border border-gray-100 p-2 cursor-pointer">
+        <div className="relative h-5 w-5 flex-shrink-0">
+          <Image
+            style={{ objectFit: 'contain' }}
+            src="https://links.papareact.com/23l"
+            fill
+            alt="Icon"
+          />
+        </div>
+        <p className="text-gray-400">Sign In</p>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
